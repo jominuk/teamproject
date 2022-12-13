6d138ad3b9f58b77d632eecabf3191285fd4f440
jhchoi1182 <jhchoi1182@gmail.com>
2022-12-13 16:49:20 +0900

    axios의 알 수 없는 modules 오류

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,7 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import nextId from "react-id-generator/lib/nextId";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import StButton from "../components/Buttons/StButton";
+import { __addTodo } from "../redux/modules/todosSlice";
 
 const AddTodo = () => {
-  return <div></div>;
+  const id = nextId();
+  const dispatch = useDispatch();
+  const [input, setInput] = useState({
+    title: "",
+    body: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (input.title.trim() === "" || input.body.trim() === "") return;
+    dispatch(
+      __addTodo({
+        id: id,
+        title: input.title,
+        body: input.body,
+        isDone: false,
+      })
+    );
+    setInput({
+      title: "",
+      body: "",
+    });
+  };
+
+  return (
+    <Addcontainer onSubmit={onSubmitHandler}>
+      <AddBox>
+        <form>
+          <TitleInput placeholder="제목을 작성해주세요" />
+          <BodoyInput placeholder="내용을 작성해주세요" />
+          <BtnBox>
+            <StButton width="13rem" height="40px" borderColor="blue" value={input.title} onChange={onChangeHandler}>
+              버킷 추가하기
+            </StButton>
+            <StButton width="13rem" height="40px" borderColor="red" value={input.body} onChange={onChangeHandler}>
+              돌아가기
+            </StButton>
+          </BtnBox>
+        </form>
+      </AddBox>
+    </Addcontainer>
+  );
 };
+
+const Addcontainer = styled.div`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AddBox = styled.div`
+  width: 60%;
+  height: 32rem;
+  background-color: teal;
+  border-radius: 8px;
+  padding: 4.5rem 5rem 5rem 5rem;
+`;
+
+const TitleInput = styled.input`
+  width: 100%;
+  height: 3rem;
+  border-radius: 10px;
+  border: 0px;
+  font-size: 2rem;
+  padding-left: 1rem;
+`;
+
+const BodoyInput = styled.textarea`
+  width: 98%;
+  height: 19rem;
+  border-radius: 10px;
+  border: 0px;
+  padding: 1rem;
+  margin-top: 3.5rem;
+  ::placeholder {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+`;
+
+const BtnBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 3rem;
+  padding: 0.5rem;
+`;
 
 export default AddTodo;
--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -1,35 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const saveData = JSON.parse(localStorage.getItem("todo"))
-  ? [...JSON.parse(localStorage.getItem("todo"))]
-  : [];
+export const __addTodos = createAsyncThunk(
+  "ADD_TODO",
+  async (todo, thunkAPI) => {
+    console.log(todo);
+    try {
+      await axios.post("http://localhost:3001/todos", todo)
+      return thunkAPI.fulfillWithValue(todo)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+    }
+  }
+)
 
 const initialState = {
-  todos: [...saveData],
+  todos: [],
   todo: {},
+  isLoading: false,
+  error: null
 };
 
 const todosSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action) => {
-      state.todos = [...state.todos, action.payload];
-    },
-    deleteTodo: (state, action) => {
-      state.todos = state.todos.filter((item) => item.id !== action.payload);
-    },
-    toggleStatusTodo: (state, action) => {
-      state.todos = state.todos.map((user) =>
-        user.id === action.payload ? { ...user, isDone: !user.isDone } : user
-      );
-    },
-    getTodoByID: (state, action) => {
-      state.todo = state.todos.filter((el) => action.payload === el.id)[0];
-    },
+    //   deleteTodo: (state, action) => {
+    //     state.todos = state.todos.filter((item) => item.id !== action.payload);
+    //   },
+    //   toggleStatusTodo: (state, action) => {
+    //     state.todos = state.todos.map((user) =>
+    //       user.id === action.payload ? { ...user, isDone: !user.isDone } : user
+    //     );
+    //   },
+    //   getTodoByID: (state, action) => {
+    //     state.todo = state.todos.filter((el) => action.payload === el.id)[0];
   },
-});
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(__addTodo.pending, (state) => {
+  //       state.isLoading = true;
+  //     })
+  //     .addCase(__addTodo.fulfillWithValue, (state, action) => {
+  //       state.isLoading = false;
+  //       state.todos = action.payload
+  //     })
+  //     .addCase(__addTodo.rejected, (state, action) => {
+  //       state.isLoading = false;
+  //       state.error = action.payload;
+  //     })
+  // }
+}
+);
 
-export const { addTodo, deleteTodo, toggleStatusTodo, getTodoByID } =
+export const { __addTodo, deleteTodo, toggleStatusTodo, getTodoByID } =
   todosSlice.actions;
 export default todosSlice.reducer;
